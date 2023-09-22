2ffd07de5869bd86d5debce5c8629253f258aa05
Amin Sadeghi <amin.sadeghi@live.com>
2023-09-21 23:16:19 -0400

    Nicer tracebacks using rich

--- autoeis/__init__.py
+++ autoeis/__init__.py
@@ -1,16 +1,16 @@
-from ._funcs import *
-
-__version__ = "0.0.15"
-__description__ = "A demo package to assist EIS analysis by automatically proposing statistically plausible ECM "
-__author__ = "Runze Zhang, Robert Black, Jason Hattrick-Simpers"
-__contact_information = "runzee.zhang@mail.utoronto.ca"
-
-
 import logging
 
+import rich.traceback
 from rich.logging import RichHandler
 
+# Enable rich traceback for nicer exception printing
+rich.traceback.install()
+
+# Set up logging using rich for nicer logging
 FORMAT = "%(message)s"
 logging.basicConfig(
-    level="WARNING", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+    level="WARNING",
+    format=FORMAT,
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)]
 )
