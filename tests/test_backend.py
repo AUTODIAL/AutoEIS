d80a70184fcfb06a77308de6d2ef0b83e7257794
Amin Sadeghi <amin.sadeghi@live.com>
2023-09-28 14:49:25 -0400

    Add basic unit tests for io and backend

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,4 +1,21 @@
-"""Unit tests for AutoEIS."""
+import pytest
+
+from autoeis import julia_helpers
+
 
 def test_init_julia():
-    pass
+    Main = julia_helpers.init_julia()
+    assert Main.eval("1+1") == 2
+
+
+def test_import_julia_modules():
+    Main = julia_helpers.init_julia()
+
+    # Ensure installed modules can be imported
+    julia_helpers.import_package(Main, "EquivalentCircuits")
+    julia_helpers.import_package(Main, "DataFrames")
+    julia_helpers.import_package(Main, "Pandas")
+
+    # Throw error for non-existent module
+    with pytest.raises(Exception):
+        julia_helpers.import_package(Main, "NonExistentModule")
