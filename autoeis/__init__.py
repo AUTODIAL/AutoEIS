66ed9f15a9d59e68e03873a9c4bed6de72bab9ff
Amin Sadeghi <amin.sadeghi@live.com>
2023-11-16 05:15:30 -0500

    Fix code lint

--- autoeis/__init__.py
+++ autoeis/__init__.py
@@ -1,5 +1,5 @@
-from . import io, utils, visualization
+from . import io, utils, visualization  # noqa: F401
 from .core import *
-from .version import __equivalent_circuits_jl_version__, __version__
+from .version import __equivalent_circuits_jl_version__, __version__  # noqa: F401
 
 utils.setup_rich_tracebacks()
