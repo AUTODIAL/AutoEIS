a62996865e425e0158e01eba896c823b5a5e4454
Amin Sadeghi <amin.sadeghi@live.com>
2023-12-04 16:23:49 -0500

    Refactor utils unit tests for clarity

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,29 +11,50 @@ y1 = x1 + np.zeros(10) * 1j
 y2 = x2 + np.zeros(10) * 1j
 
 
-def test_mse_score():
-    # Real numbers
-    assert utils.mse_score(x1, x2) == skmetrics.mean_squared_error(x1, x2)
-    # Complex numbers
-    assert utils.mse_score(y1, y2) == skmetrics.mean_squared_error(x1, x2)
+def test_mse_score_real():
+    mse = utils.mse_score(x1, x2)
+    mse_gt = skmetrics.mean_squared_error(x1, x2)
+    assert np.isclose(mse, mse_gt)
 
 
-def test_rmse_score():
-    # Real numbers
-    assert utils.rmse_score(x1, x2) == skmetrics.mean_squared_error(x1, x2, squared=False)
-    # Complex numbers
-    assert utils.rmse_score(y1, y2) == skmetrics.mean_squared_error(x1, x2, squared=False)
+def test_mse_score_complex():
+    mse = utils.mse_score(y1, y2)
+    mse_gt = skmetrics.mean_squared_error(x1, x2)
+    assert np.isclose(mse, mse_gt)
 
 
-def test_mape_score():
-    # Real numbers
-    assert utils.mape_score(x1, x2) == skmetrics.mean_absolute_percentage_error(x1, x2) * 100
-    # Complex numbers
-    assert utils.mape_score(y1, y2) == skmetrics.mean_absolute_percentage_error(x1, x2) * 100
+def test_rmse_score_real():
+    rmse = utils.rmse_score(x1, x2)
+    rmse_gt = skmetrics.mean_squared_error(x1, x2, squared=False)
+    assert np.isclose(rmse, rmse_gt)
 
 
-def test_r2_score():
-    # Real numbers
-    assert utils.r2_score(x1, x2) == skmetrics.r2_score(x1, x2)
-    # Complex numbers
-    assert utils.r2_score(y1, y2) == skmetrics.r2_score(x1, x2)
+def test_rmse_score_complex():
+    rmse = utils.rmse_score(y1, y2)
+    rmse_gt = skmetrics.mean_squared_error(x1, x2, squared=False)
+    assert np.isclose(rmse, rmse_gt)
+
+
+def test_mape_score_real():
+    mape = utils.mape_score(x1, x2)
+    mape_gt = skmetrics.mean_absolute_percentage_error(x1, x2) * 100
+    assert np.isclose(mape, mape_gt)
+
+
+def test_mape_score_complex():
+    mape = utils.mape_score(y1, y2)
+    mape_gt = skmetrics.mean_absolute_percentage_error(x1, x2) * 100
+    assert np.isclose(mape, mape_gt)
+
+
+def test_r2_score_real():
+    r2 = utils.r2_score(x1, x2)
+    r2_gt = skmetrics.r2_score(x1, x2)
+    assert np.isclose(r2, r2_gt)
+
+
+def test_r2_score_complex():
+    r2 = utils.r2_score(y1, y2)
+    r2_gt = skmetrics.r2_score(x1, x2)
+    assert np.isclose(r2, r2_gt)
+    
