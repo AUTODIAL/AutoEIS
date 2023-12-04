096cfe1658554048185bd10589a85eec890d4c36
Amin Sadeghi <amin.sadeghi@live.com>
2023-12-04 16:23:58 -0500

    Add unit test for `fit_circuit_parameters`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sklearn.metrics as skmetrics
+from impedance.models.circuits import CustomCircuit
 
 from autoeis import utils
 
@@ -9,6 +10,17 @@ x2 = np.random.rand(10)
 # Complex numbers
 y1 = x1 + np.zeros(10) * 1j
 y2 = x2 + np.zeros(10) * 1j
+
+# Simulated EIS data
+circuit_string = "R1-[P2,R3]"
+parameters = np.array([250, 1e-3, 0.5, 10])
+circuit = CustomCircuit(
+    utils.impedancepy_circuit(circuit_string),
+    initial_guess=parameters
+)
+circuit.parameters_ = parameters
+freq = np.logspace(-3, 3, 1000)
+Z = circuit.predict(freq)
 
 
 def test_mse_score_real():
@@ -58,3 +70,15 @@ def test_r2_score_complex():
     r2_gt = skmetrics.r2_score(x1, x2)
     assert np.isclose(r2, r2_gt)
     
+
+def test_fit_circuit_parameters_no_initial_guess():
+    params_dict = utils.fit_circuit_parameters(circuit_string, Z, freq)
+    parameters_fit = np.array(list(params_dict.values()))
+    assert np.allclose(parameters_fit, parameters, rtol=0.01)
+
+
+def test_fit_circuit_parameters_with_initial_guess():
+    p0 = parameters + np.random.rand(len(parameters)) * parameters * 0.5
+    params_dict = utils.fit_circuit_parameters(circuit_string, Z, freq, p0)
+    parameters_fit = np.array(list(params_dict.values()))
+    assert np.allclose(parameters_fit, parameters, rtol=0.01)
