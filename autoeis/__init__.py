da199470be6b3f04a8b28b3e7feee852b2e47065
Amin Sadeghi <amin.sadeghi@live.com>
2023-12-10 09:15:04 -0500

    Override print with rich.print

--- autoeis/__init__.py
+++ autoeis/__init__.py
@@ -1,5 +1,11 @@
+import builtins
+
 from . import core, io, utils, visualization  # noqa: F401
 from .core import *
 from .version import __equivalent_circuits_jl_version__, __version__  # noqa: F401
 
+# Set up rich traceback for nicer exception printing
 utils.setup_rich_tracebacks()
+
+# Override print() with rich's print() for nicer printing
+builtins.print = visualization.rich_print
