2a763a6bbdae9dfc7192523e03b117b86d2a9e71
Amin Sadeghi <amin.sadeghi@live.com>
2024-06-20 13:05:41 -0400

    test: Add unit tests for SCSD, MCSD, SCMD inferences

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,53 +1,52 @@
+import autoeis as ae
 import numpy as np
-import numpyro
 import pandas as pd
 import pytest
-from autoeis import core, io, utils
 
 
 def test_compute_ohmic_resistance():
     circuit_string = "R1-[P2,P3-R4]"
-    circuit_fn = utils.generate_circuit_fn_impedance_backend(circuit_string)
+    circuit_fn = ae.utils.generate_circuit_fn_impedance_backend(circuit_string)
     R1 = 250
     parameters = np.array([R1, 1e-3, 0.1, 5e-5, 0.8, 10])
     freq = np.logspace(-3, 3, 1000)
     Z = circuit_fn(freq, parameters)
-    R = core.compute_ohmic_resistance(freq, Z)
+    R = ae.core.compute_ohmic_resistance(freq, Z)
     np.testing.assert_allclose(R, R1, rtol=0.15)
 
 
 def test_compute_ohmic_resistance_missing_high_freq():
     circuit_string = "R1-[P2,P3-R4]"
-    circuit_fn = utils.generate_circuit_fn_impedance_backend(circuit_string)
+    circuit_fn = ae.utils.generate_circuit_fn_impedance_backend(circuit_string)
     R1 = 250
     parameters = np.array([R1, 1e-3, 0.1, 5e-5, 0.8, 10])
     freq = np.logspace(-3, 0, 1000)
     Z = circuit_fn(freq, parameters)
-    R = core.compute_ohmic_resistance(freq, Z)
+    R = ae.core.compute_ohmic_resistance(freq, Z)
     # When high frequency measurements are missing, Re(Z) @ max(freq) is good approximation
     Zreal_at_high_freq = Z.real[np.argmax(freq)]
     np.testing.assert_allclose(R, Zreal_at_high_freq)
 
 
 def test_preprocess_impedance_data():
-    freq, Z = io.load_test_dataset()
+    freq, Z = ae.io.load_test_dataset()
     # Test various tolerances for linKK validation
-    freq_prep, Z_prep = core.preprocess_impedance_data(freq, Z, tol_linKK=5e-2)
+    freq_prep, Z_prep = ae.utils.preprocess_impedance_data(freq, Z, tol_linKK=5e-2)
     assert len(Z_prep) == len(freq_prep)
     assert len(Z_prep) == 60
-    freq_prep, Z_prep = core.preprocess_impedance_data(freq, Z, tol_linKK=5e-3)
+    freq_prep, Z_prep = ae.utils.preprocess_impedance_data(freq, Z, tol_linKK=5e-3)
     assert len(Z_prep) == len(freq_prep)
     assert len(Z_prep) == 50
     # Test return_aux=True
-    _, _, aux = core.preprocess_impedance_data(freq, Z, return_aux=True)
+    _, _, aux = ae.utils.preprocess_impedance_data(freq, Z, return_aux=True)
     assert list(aux.keys()) == ["res", "rmse"]
     assert list(aux["res"].keys()) == ["real", "imag"]
 
 
 def test_gep():
     def test_gep_serial():
-        freq, Z = io.load_test_dataset()
-        freq, Z = core.preprocess_impedance_data(freq, Z, tol_linKK=5e-2)
+        freq, Z = ae.io.load_test_dataset()
+        freq, Z = ae.utils.preprocess_impedance_data(freq, Z, tol_linKK=5e-2)
         kwargs = {
             "iters": 2,
             "complexity": 2,
@@ -56,13 +55,12 @@ def test_gep():
             "tol": 1e10,
             "parallel": False,
         }
-        circuits = core.generate_equivalent_circuits(freq, Z, **kwargs)
+        circuits = ae.core.generate_equivalent_circuits(freq, Z, **kwargs)
         assert len(circuits) == kwargs["iters"]
         assert isinstance(circuits, pd.DataFrame)
 
     def test_gep_parallel():
-        freq, Z = io.load_test_dataset()
-        freq, Z = core.preprocess_impedance_data(freq, Z, tol_linKK=5e-2)
+        freq, Z = ae.io.load_test_dataset(preprocess=True)
         kwargs = {
             "iters": 2,
             "complexity": 2,
@@ -71,7 +69,7 @@ def test_gep():
             "tol": 1e10,
             "parallel": True,
         }
-        circuits = core.generate_equivalent_circuits(freq, Z, **kwargs)
+        circuits = ae.core.generate_equivalent_circuits(freq, Z, **kwargs)
         assert len(circuits) == kwargs["iters"]
         assert isinstance(circuits, pd.DataFrame)
 
@@ -80,43 +78,57 @@ def test_gep():
 
 
 def test_filter_implausible_circuits():
-    circuits_unfiltered = io.load_test_circuits()
+    circuits_unfiltered = ae.io.load_test_circuits()
     N1 = len(circuits_unfiltered)
-    circuits = core.filter_implausible_circuits(circuits_unfiltered)
+    circuits = ae.core.filter_implausible_circuits(circuits_unfiltered)
     N2 = len(circuits)
     assert N2 < N1
 
 
-def test_bayesian_inference_single():
-    freq, Z = io.load_test_dataset()
-    freq, Z = core.preprocess_impedance_data(freq, Z, tol_linKK=5e-2)
-    circuits = io.load_test_circuits(filtered=True)
+def test_bayesian_inference_single_circuit_single_data():
+    freq, Z = ae.io.load_test_dataset(preprocess=True)
+    circuits = ae.io.load_test_circuits(filtered=True)
     circuit = circuits.iloc[0].circuitstring
     p0 = circuits.iloc[0].Parameters
     kwargs_mcmc = {"num_warmup": 25, "num_samples": 10, "progress_bar": False}
-    results = core.perform_bayesian_inference(circuit, freq, Z, p0, **kwargs_mcmc)
-    mcmc, exit_code = results[0]
-    assert exit_code in [-1, 0]
-    assert isinstance(mcmc, numpyro.infer.mcmc.MCMC)
+    result = ae.core.perform_bayesian_inference(circuit, freq, Z, p0, **kwargs_mcmc)
+    assert isinstance(result, ae.utils.InferenceResult)
+    assert result.converged
 
 
-def test_bayesian_inference_batch():
-    freq, Z = io.load_test_dataset()
-    freq, Z = core.preprocess_impedance_data(freq, Z, tol_linKK=5e-2)
-    # Test the first two circuits to save CI time
-    circuits = io.load_test_circuits(filtered=True).iloc[:2]
+def test_bayesian_inference_single_circuit_multiple_data():
+    # Load test dataset N times with noise to simulate multiple datasets
+    n_datasets = 2
+    freq, Z = zip(
+        *[ae.io.load_test_dataset(preprocess=True, noise=0.1) for _ in range(n_datasets)]
+    )
+    circuits = ae.io.load_test_circuits(filtered=True)
+    circuit = circuits.iloc[0].circuitstring
+    p0 = circuits.iloc[0].Parameters
     kwargs_mcmc = {"num_warmup": 25, "num_samples": 10, "progress_bar": False}
-    results = core.perform_bayesian_inference(circuits, freq, Z, **kwargs_mcmc)
+    results = ae.core.perform_bayesian_inference(circuit, freq, Z, p0, **kwargs_mcmc)
+    assert len(results) == n_datasets
+    for result in results:
+        assert isinstance(result, ae.utils.InferenceResult)
+        assert result.converged
+
+
+def test_bayesian_inference_multiple_circuits_single_data():
+    freq, Z = ae.io.load_test_dataset(preprocess=True)
+    circuits = ae.io.load_test_circuits(filtered=True)
+    circuits = circuits.iloc[:2]  # Test the first two circuits to save CI time
+    kwargs_mcmc = {"num_warmup": 25, "num_samples": 10, "progress_bar": False}
+    results = ae.core.perform_bayesian_inference(circuits, freq, Z, **kwargs_mcmc)
     assert len(results) == len(circuits)
-    for mcmc, exit_code in results:
-        assert exit_code == 0  # Ensure convergence
-        assert isinstance(mcmc, numpyro.infer.mcmc.MCMC)  # Ensure correct type
+    for result in results:
+        assert isinstance(result, ae.utils.InferenceResult)
+        assert result.converged
 
 
 # TODO: Use a simple circuit to generate test data so this test doesn't take too long
 @pytest.mark.skip(reason="This test is too slow!")
 def test_perform_full_analysis():
-    freq, Z = io.load_test_dataset()
-    results = core.perform_full_analysis(freq, Z)
+    freq, Z = ae.io.load_test_dataset()
+    results = ae.core.perform_full_analysis(freq, Z)
     required_columns = ["circuitstring", "Parameters", "MCMC", "success", "divergences"]
     assert all(col in results.columns for col in required_columns)
