80e2df368f3c61f9d3278fdb3382e293e1c43fe8
Runze Zhang <97633833+RunzeZhang123@users.noreply.github.com>
2025-07-03 13:17:59 -0400

    Update invoke.sh

--- middleware/invoke.sh
+++ middleware/invoke.sh
@@ -3,7 +3,7 @@
 # jupyter
 #
 /usr/bin/invoke_app "$@" -t autoeis \
-                        -C "start_jupyter -T @tool <AutoEIS_workflow.ipynb>" \
+                        -C "start_jupyter -T @tool AutoEIS_workflow.ipynb" \
                         -u anaconda-7 \
                         -r none \
                         -w headless
